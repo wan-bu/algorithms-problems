1a0e2c39ffb831b8af5ee5324744d17e4a11b2c6
Marouane BOUKEDIR <marouaneboukedir@gmail.com>
2020-09-30 19:39:54 +0100

    two sum naive solution

---
+++ TwoSum.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {
+            10,
+            16,
+            3,
+            7
+        };
+        int k = 25;
+        System.out.println(isTwoElementsAddUpTo(numbers, k));
+    }
+    static boolean isTwoElementsAddUpTo(int[] numbers, int k) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
